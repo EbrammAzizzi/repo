bc3327f86b6185522abd5bc4d45283dfdb62f51e
EbrammAzizzi <138739241+EbrammAzizzi@users.noreply.github.com>
2024-01-10 19:04:30 +0300

    Update de.js

--- de.js
+++ de.js
@@ -2,7 +2,7 @@ int a = 482
 
 class Animal {
   public void animalSound() {
-    System.out.println("The animal makeeds a psound");
+    System.out.println("The animal makeeds a psssound");
   }
 }
 
