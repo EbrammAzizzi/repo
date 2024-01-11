474270761eb4e069f41c825b559dbe6bdf7fcbec
EbrammAzizzi <138739241+EbrammAzizzi@users.noreply.github.com>
2024-01-11 18:26:26 +0300

    Update de.js

--- de.js
+++ de.js
@@ -21,7 +21,7 @@ class Dog extends Animal {
 class Main {
   public static void main(String[] args) {
     Animal myAnimal = new Animal();  // Create a Animal object
-    Animal myPig = new Pig();  // Create a Pig object
+    Animal myPig = new Pig(22);  // Create a Pig object
     Animal myDog = new Dog();  // Create a Dog object
     myAnimal.animalSound();
     myPig.animalSound();
