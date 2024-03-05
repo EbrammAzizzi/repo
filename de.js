618e821a16828c8c180bd97026955042262c59a0
EbrammAzizzi <138739241+EbrammAzizzi@users.noreply.github.com>
2024-03-05 16:29:15 +0300

    Update de.js

--- de.js
+++ de.js
@@ -14,7 +14,7 @@ class Pig extends Animal {
 
 class Dog extends Animal {
   public void animalSound() {
-    System.out.println("The dog sas: bow wow");
+    System.out.println("The dog as: bow wow");
   }
 }
 
